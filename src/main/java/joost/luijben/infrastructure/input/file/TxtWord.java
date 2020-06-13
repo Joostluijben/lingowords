69c33f2eddb18a9034198f717952649e7182ead0
Joost Luijben <joost.luijben@student.hu.nl>
2020-06-13 16:55:01 +0200

    Put the words file on classpath so maven can use it

--- src/main/java/joost/luijben/infrastructure/input/file/TxtWord.java
+++ src/main/java/joost/luijben/infrastructure/input/file/TxtWord.java
@@ -1,12 +1,13 @@
 package joost.luijben.infrastructure.input.file;
 
 import joost.luijben.domain.Word;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Objects;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -15,13 +16,13 @@ public class TxtWord implements WordDeserializer{
 
     @Override
     public Set<Word> getWords() {
-        File file = new File(Objects.requireNonNull(getClass().getClassLoader().getResource("basiswoorden-gekeurd.txt")).getFile());
         try {
-            return Files.lines(file.toPath()).map(Word::new).collect(Collectors.toSet());
+            InputStream inputStream = new ClassPathResource("basiswoorden-gekeurd.txt").getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            return bufferedReader.lines().map(Word::new).collect(Collectors.toSet());
         } catch (IOException e) {
-            //TODO foutafhandeling
             e.printStackTrace();
+            return null;
         }
-        return Set.of();
     }
 }
